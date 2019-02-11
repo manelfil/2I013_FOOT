0a311ef3e4f78bc0dcc01b41fda349b0d8bf84fb
RAUA <3700629@ppti-14-303-02.ufr-info-p6.jussieu.fr>
2019-02-11 17:46:06 +0100

    tme4

---
+++ fic1.py
@@ -0,0 +1,42 @@
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu
+from soccersimulator import Strategy
+from soccersimulator import settings
+import math
+from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam, Simulation, show_simu
+
+
+class RandomStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Random")
+
+    def compute_strategy(self, state, id_team, id_player):
+        # id_team is 1 or 2
+        # id_player starts at 0
+        return SoccerAction(Vector2D.create_random(),
+                            Vector2D.create_random())
+
+class OOStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "OO")
+
+    def compute_strategy(self, state, id_team, id_player):
+        # id_team is 1 or 2
+        # id_player starts at 0
+        return SoccerAction(Vector2D.create_random(),
+                            Vector2D.create_random())
+
+
+# Create teams
+#team1 = SoccerTeam(name="Team 1")
+#team2 = SoccerTeam(name="Team 2")
+
+# Add players
+#team1.add("Random", RandomStrategy())  # Random strategy
+#team2.add("Static", Strategy())   # Static strategy
+
+# Create a match
+#simu = Simulation(team1, team2)
+
+# Simulate and display the match
+#show_simu(simu)
--- tournament.py
+++ tournament.py
@@ -20,4 +20,5 @@ if __name__ == '__main__':
     team1= get_team(1)
     team2= get_team(2)
 
-    
+    simu = Simulation( team1, team2)
+    show_simu(simu)
