a86ffec6bd60e774ec412b61e47644da37f618f1
RAUA <3700629@ppti-14-307-03.ufr-info-p6.jussieu.fr>
2019-02-18 12:30:41 +0100

    tme5

--- tournament.py
+++ tournament.py
@@ -1,23 +1,9 @@
-from socceria.Supertool import RandomStrategy,Fonceur_Strategy,Defenseur_Strategy
-from soccersimulator import SoccerTeam
-
-def get_team(nb_players):
-    team= SoccerTeam(name="KhadijaManel's Team")
-    if(nb_players==1):
-        print("team created")
-        team.add("Striker",Fonceur_Strategy())
-    if(nb_players==2):
-        team.add("Striker",Fonceur_Strategy())
-        team.add("Random",Defenseur_Strategy())
-
-    return team
-
-
-if __name__ == '__main__':
-    from soccersimulator import Simulation, show_simu
-    print("iii")
-	
-    team1= get_team(1)
-    team2= get_team(2)
-
+from SocceriaMK import get_team
+from soccersimulator import Simulation, show_simu
     
+	
+team1= get_team(1)
+team2= get_team(2)
+
+simu = Simulation( team1, team2)
+show_simu(simu)
